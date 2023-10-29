a3bfa096e2701d1de70f254038fdef7a8cec68d3
a7mad1112 <ahmalawneh79@gmail.com>
2023-10-29 17:17:25 +0200

    get all categories endpoint

--- src/modules/categories/categories.controllre.js
+++ src/modules/categories/categories.controllre.js
@@ -2,8 +2,9 @@ import slugify from 'slugify';
 import cloudinary from '../../services/cloudinary.js'
 import categoryModel from './../../../db/models/category.model.js';
 
-export const getCategories = (req, res) => {
-  return res.json("categories");
+export const getCategories = async (req, res) => {
+  const categories = await categoryModel.find();
+  return res.status(200).json({ msg: 'Success', categories });
 }
 
 export const createCategory = async (req, res) => {
@@ -13,10 +14,10 @@ export const createCategory = async (req, res) => {
   const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path, {
     folder: `${process.env.APP_NAME}/categories`
   });
-  const categ = await categoryModel.create({
+  const category = await categoryModel.create({
     name, slug: slugify(name), image: {
       secure_url, public_id
     }
   });
-  return res.status(201).json({ msg: 'Success', category: categ });
+  return res.status(201).json({ msg: 'Success', category: category });
 }
