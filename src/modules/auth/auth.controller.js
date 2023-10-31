b60285fd4ab6cef1bbfe4c60a24c2ccbb7830b49
a7mad1112 <ahmalawneh79@gmail.com>
2023-10-31 22:42:35 +0200

    generate token for loing

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../../../db/models/user.model.js";
 import bcrypt from 'bcryptjs';
 import cloudinary from './../../services/cloudinary.js';
+import jwt from "jsonwebtoken";
 export const signup = async (req, res) => {
   const { body: { userName, email, password }, file } = req;
   const user = await userModel.findOne({ email });
@@ -31,6 +32,14 @@ export const signin = async (req, res) => {
     return res.status(404).json({ msg: "User not found" });
   }
 
+  const token = jwt.sign({
+    id: user._id, role: user.role, status: user.status
+  },
+    process.env.LOGIN_SECRET_KEY, { expiresIn: '5m' });
 
-  return res.status(200).json({ msg: "Success", user });
-}
+  const refreshToekn = jwt.jwt.sign({
+    id: user._id, role: user.role, status: user.status
+  },
+    process.env.LOGIN_SECRET_KEY, { expiresIn: 60 * 60 * 24 * 30 });
+  return res.status(200).json({ msg: "Success", token, refreshToekn });
+};
