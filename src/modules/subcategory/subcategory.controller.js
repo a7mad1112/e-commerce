2e8cdaadf079cc668d62327c131fffa0fd2a9496
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-05 19:49:32 +0200

    Fix wrong file accesing in createSubCategory controller

--- src/modules/subcategory/subcategory.controller.js
+++ src/modules/subcategory/subcategory.controller.js
@@ -1,22 +1,23 @@
 import categoryModel from '../../../db/models/category.model.js';
 import subCategoryModel from './../../../db/models/subcategory.model.js';
 import cloudinary from './../../services/cloudinary.js';
+import slugify from 'slugify';
 export const createSubCategory = async (req, res) => {
   const { name, categoryId } = req.body;
   const subcategory = await subCategoryModel.findOne({ name });
   if (subcategory) {
     return res.status(409).json({ msg: "Sub Category  " + name + " already exist" });
   }
-  const category = await subCategoryModel.findById(categoryId);
+  const category = await categoryModel.findById(categoryId);
   if (!category) {
     return res.status(404).json({ msg: "Category not found" });
   }
-  const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, {
+  console.log(req.file)
+  const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path, {
     folder: `${process.env.APP_NAME}/sub-categories`
   });
   const subCategory = await subCategoryModel.create({ name, slug: slugify(name), categoryId, image: { secure_url, public_id } })
   return res.status(201).json({ msg: "Success", subCategory });
-
 };
 
 export const getSubCategories = async (req, res) => {
