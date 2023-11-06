1fbb6f7e4dd33c69d087fe0d19e7abcb338c68c7
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-06 21:40:07 +0200

    create coupon endpoint

--- src/modules/coupon/coupon.controller.js
+++ src/modules/coupon/coupon.controller.js
@@ -1,1 +1,8 @@
-export const createCoupon = (req, res) => res.json("sdaf");
+import couponModel from './../../../db/models/coupon.model.js';
+export const createCoupon = async (req, res) => {
+  const { name } = req.body;
+  if (await couponModel.findOne({ name }))
+    return res.status(409).json({ msg: "Coupon name already exist" })
+  const coupon = await couponModel.create(req.body);
+  return res.status(201).json({ msg: "Success", coupon });
+};
