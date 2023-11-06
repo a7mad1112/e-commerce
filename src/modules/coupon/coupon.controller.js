874ff6c5cb614d9604f23e6c0d8f4f501a5b5cd4
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-06 21:58:38 +0200

    update coupon endpoint

--- src/modules/coupon/coupon.controller.js
+++ src/modules/coupon/coupon.controller.js
@@ -10,3 +10,22 @@ export const getCoupons = async (req, res) => {
   const coupons = await couponModel.find({});
   return res.status(200).json({ msg: "Success", coupons });
 }
+export const updateCoupon = async (req, res) => {
+  const { params: { id }, body: { name, amount } } = req;
+  const coupon = await couponModel.findById(id);
+  if (!coupon) {
+    return res.status(404).json({ msg: "Coupon not found" });
+  }
+  if (name) {
+    if (await couponModel.findOne({ name }).select('name')) {
+      return res.status(409).json({ msg: "Name already exists" });
+    }
+    coupon.name = name;
+  }
+
+  if (amount) {
+    coupon.amount = amount;
+  }
+  await coupon.save();
+  return res.status(200).json({ msg: "Success", coupon });
+}
--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -4,5 +4,6 @@ const router = Router();
 
 router.post('/', couponController.createCoupon)
 router.get('/', couponController.getCoupons)
+router.put('/:id', couponController.updateCoupon)
 
 export default router;
