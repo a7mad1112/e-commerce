34cc1d6d4d57d9c78733ec69e7c0ab6aee399072
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-08 21:54:30 +0200

    Add category roles

--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -3,14 +3,15 @@ import * as categoriesContoller from './categories.controller.js'
 import fileUpload, { fileValidation } from '../../services/multer.js'
 import subCategoryRouter from '../subcategory/subcategory.router.js'
 import { auth } from '../../middleware/auth.middleware.js';
+import { endPoints } from './categories.roles.js';
 const router = Router();
 
 router.use("/:id/subcategory", subCategoryRouter)
-router.get('/', auth(["Admin"]), categoriesContoller.getCategories);
-router.get('/active', auth(["User", "Admin"]), categoriesContoller.getActiveCategories);
-router.get('/:id', categoriesContoller.getSpecficCategory);
-router.post('/', fileUpload(fileValidation.image).single('image'),
+router.get('/', auth(endPoints.getAll), categoriesContoller.getCategories);
+router.get('/active', auth(endPoints.getActive), categoriesContoller.getActiveCategories);
+router.get('/:id', auth(endPoints.specific), categoriesContoller.getSpecficCategory);
+router.post('/', auth(endPoints.create), fileUpload(fileValidation.image).single('image'),
   categoriesContoller.createCategory);
-router.put('/:id', fileUpload(fileValidation.image).single('image'),
+router.put('/:id', auth(endPoints.update), fileUpload(fileValidation.image).single('image'),
   categoriesContoller.updateCategory);
 export default router;
