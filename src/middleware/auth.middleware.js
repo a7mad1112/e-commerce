a61ef964b9589f178c1a46da7f8109f7b239edea
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-11 17:20:13 +0200

    Fix auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,22 +1,40 @@
 import jwt from "jsonwebtoken";
 import userModel from './../../db/models/user.model.js';
-export const auth = async (accessRoles = []) => {
+
+export const auth = (accessRoles = []) => {
   return async (req, res, next) => {
-    const { BEARAR_KEY, LOGIN_SECRET_KEY } = process.env;
-    const { authorization } = req.headers;
-    if (!authorization.startsWith(BEARAR_KEY)) {
-      return res.status(400).json({ msg: "Invalid authorization" });
+    try {
+      const { BEARAR_KEY, LOGIN_SECRET_KEY } = process.env;
+      const { authorization } = req.headers;
+
+      if (!authorization || !authorization.startsWith(BEARAR_KEY)) {
+        return res.status(400).json({ msg: "Invalid or missing authorization header" });
+      }
+
+      const token = authorization.split(BEARAR_KEY)[1];
+      if (!token) {
+        return res.status(400).json({ msg: "Invalid token" });
+      }
+
+      const decoded = jwt.verify(token, LOGIN_SECRET_KEY);
+      if (!decoded) {
+        return res.status(400).json({ msg: "Invalid authorization token" });
+      }
+
+      const user = await userModel.findById(decoded.id);
+      if (!user) {
+        return res.status(404).json({ msg: "User not found" });
+      }
+
+      if (!accessRoles.includes(decoded.role)) {
+        return res.status(403).json({ msg: "Not authorized to access this resource" });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error("Authentication error:", error);
+      return res.status(500).json({ msg: "Internal server error during authentication" });
     }
-    const token = authorization.split(BEARAR_KEY)[1];
-    const decoded = jwt.verify(token, LOGIN_SECRET_KEY);
-    if (!decoded) {
-      return res.status(400).json({ msg: "Invalid authorization" });
-    }
-    const user = await userModel.findById(decoded.id)
-    if (!accessRoles.includes(decoded.role)) {
-      return res.status(403).json({ msg: "Not auth user" });
-    }
-    req.user = user;
-    next();
-  }
-}
+  };
+};
--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -6,12 +6,12 @@ import { auth } from '../../middleware/auth.middleware.js';
 import { endPoints } from './categories.roles.js';
 const router = Router();
 
-// router.use("/:id/subcategory", subCategoryRouter)
-// router.get('/', auth(endPoints.getAll), categoriesContoller.getCategories);
-// router.get('/active', auth(endPoints.getActive), categoriesContoller.getActiveCategories);
-// router.get('/:id', auth(endPoints.specific), categoriesContoller.getSpecficCategory);
-// router.post('/', auth(endPoints.create), fileUpload(fileValidation.image).single('image'),
-//   categoriesContoller.createCategory);
-// router.put('/:id', auth(endPoints.update), fileUpload(fileValidation.image).single('image'),
-//   categoriesContoller.updateCategory);
+router.use("/:id/subcategory", subCategoryRouter)
+router.get('/', auth(endPoints.getAll), categoriesContoller.getCategories);
+router.get('/active', auth(endPoints.getActive), categoriesContoller.getActiveCategories);
+router.get('/:id', auth(endPoints.specific), categoriesContoller.getSpecficCategory);
+router.post('/', auth(endPoints.create), fileUpload(fileValidation.image).single('image'),
+  categoriesContoller.createCategory);
+router.put('/:id', auth(endPoints.update), fileUpload(fileValidation.image).single('image'),
+  categoriesContoller.updateCategory);
 export default router;
