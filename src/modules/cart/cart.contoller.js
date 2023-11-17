c4b47fb0cf33a0c8134c04f36d89ef3d7d608118
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-17 17:36:26 +0200

    Create, update quantity, add product end-point

--- src/modules/cart/cart.contoller.js
+++ src/modules/cart/cart.contoller.js
@@ -1,3 +1,22 @@
-export const createCart = (req, res) => {
-  return res.json("OKJJ");
+import cartModel from './../../../db/models/cart.model.js';
+export const createCart = async (req, res) => {
+  const { productId, quantity } = req.body;
+  const cart = await cartModel.findOne({ userId: req.user._id });
+  if (!cart) {
+    const newCart = await cartModel.create({
+      userId: req.user._id,
+      products: { productId, quantity },
+    });
+    return res.status(201).json({ msg: "Success", cart: newCart });
+  }
+  for (let i = 0; i < cart.products.length; i++) {
+    if (cart.products[i].productId == productId) {
+      cart.products[i].quantity = quantity;
+      await cart.save();
+      return res.status(200).json({ msg: "Quantity updated in the user's cart", cart });
+    }
+  }
+  cart.products.push({ productId, quantity });
+  await cart.save();
+  return res.status(201).json({ msg: "New product added to the cart", cart });
 };
