689e7d11b90502b47d9b117f0a6b8e71d54f4a6c
a7mad1112 <ahmalawneh79@gmail.com>
2023-12-18 16:47:17 +0200

    upload excel file endpoint

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,6 +1,17 @@
-import userModel from "../../../db/models/user.model.js";
+import userModel from '../../../db/models/user.model.js';
+import XLSX from 'xlsx';
 
 export const getProfile = async (req, res) => {
   const user = await userModel.findById(req.user._id);
-  return res.status(200).json({ msg: "Success", user });
-}
+  return res.status(200).json({ msg: 'Success', user });
+};
+
+export const uploadUserExcel = async (req, res, next) => {
+  const woorkBook = XLSX.readFile(req.file.path);
+  const woorkSheet = woorkBook.Sheets[woorkBook.SheetNames[0]];
+  const users = XLSX.utils.sheet_add_json(woorkSheet);
+  if (!(await userModel.insertMany(users))) {
+    return next(new Error('invalid'));
+  }
+  return res.status(201).json({ msg: 'success' });
+};
--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,10 +1,19 @@
-import { Router } from "express";
+import { Router } from 'express';
 import { auth } from './../../middleware/auth.middleware.js';
 import { roles } from './../../roles.js';
 import * as userController from './user.controller.js';
 import { asyncHandler } from './../../utils/errorHandler.js';
+import fileUpload, { fileValidation } from '../../utils/multer.js';
 const router = Router();
 
-router.get('/profile', auth(Object.values(roles)),
-  asyncHandler(userController.getProfile));
+router.get(
+  '/profile',
+  auth(Object.values(roles)),
+  asyncHandler(userController.getProfile)
+);
+router.post(
+  '/upload-user-excel',
+  auth(['Admin'], fileUpload(fileValidation.excel).single('file')),
+  asyncHandler(userController.uploadUserExcel)
+);
 export default router;
