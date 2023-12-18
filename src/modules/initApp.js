5712bae7bc8fc31002c1c1923315447547c218a1
a7mad1112 <ahmalawneh79@gmail.com>
2023-12-18 16:21:32 +0200

    adding cors

--- src/modules/initApp.js
+++ src/modules/initApp.js
@@ -1,26 +1,28 @@
-import productsRouter from './products/products.router.js'
-import categoriesRouter from './categories/categories.router.js'
-import connectDB from '../../db/connection.js'
-import authRouter from '../modules/auth/auth.router.js'
-import subCategoryRouter from './subcategory/subcategory.router.js'
-import couponRouter from './coupon/coupon.router.js'
-import cartRouter from '../modules/cart/cart.router.js'
-import orderRouter from '../modules/order/order.router.js'
-import { globalErrorHandler } from '../utils/errorHandler.js'
+import productsRouter from './products/products.router.js';
+import categoriesRouter from './categories/categories.router.js';
+import connectDB from '../../db/connection.js';
+import authRouter from '../modules/auth/auth.router.js';
+import subCategoryRouter from './subcategory/subcategory.router.js';
+import couponRouter from './coupon/coupon.router.js';
+import cartRouter from '../modules/cart/cart.router.js';
+import orderRouter from '../modules/order/order.router.js';
+import { globalErrorHandler } from '../utils/errorHandler.js';
 import userRouter from '../modules/user/user.router.js';
+import cors from 'cors';
 const initApp = async (app, express) => {
-  connectDB()
-  app.use(express.json())
-  app.use('/products', productsRouter)
-  app.use('/categories', categoriesRouter)
-  app.use('/auth', authRouter)
-  app.use('/subcategory', subCategoryRouter)
-  app.use('/coupon', couponRouter)
+  app.use(cors());
+  connectDB();
+  app.use(express.json());
+  app.use('/products', productsRouter);
+  app.use('/categories', categoriesRouter);
+  app.use('/auth', authRouter);
+  app.use('/subcategory', subCategoryRouter);
+  app.use('/coupon', couponRouter);
   app.use('/cart', cartRouter);
   app.use('/order', orderRouter);
   app.use('/user', userRouter);
-  app.use('*', (req, res) => res.status(404).json({ msg: "page not found" }))
-  app.use(globalErrorHandler)
-}
+  app.use('*', (req, res) => res.status(404).json({ msg: 'page not found' }));
+  app.use(globalErrorHandler);
+};
 
 export default initApp;
